8f940c0152e334295fa9c61f0a3912e7d254ab8d
Tyyagoo <kinuchiha87@gmail.com>
2022-03-29 12:39:29 -0300

    :tada: initial commit

---
+++ lib/issuex.ex
@@ -0,0 +1,18 @@
+defmodule Issuex do
+  @moduledoc """
+  Documentation for `Issuex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Issuex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/issuex_test.exs
@@ -0,0 +1,8 @@
+defmodule IssuexTest do
+  use ExUnit.Case
+  doctest Issuex
+
+  test "greets the world" do
+    assert Issuex.hello() == :world
+  end
+end
